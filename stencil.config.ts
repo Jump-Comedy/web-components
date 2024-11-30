2a0dff76b6879aaf032d7811808a3f154f8f3c5e
Arsenalist <zarars@gmail.com>
2024-11-29 19:07:01 -0500

    es5 builds

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,13 +1,18 @@
-import { Config } from '@stencil/core';
+import { Config } from "@stencil/core";
 
 export const config: Config = {
-  namespace: 'web-components',
+  namespace: "web-components",
+  buildEs5: true,
   outputTargets: [
     {
-      type: 'dist-custom-elements',
-      customElementsExportBehavior: 'auto-define-custom-elements',
+      type: "dist-custom-elements",
+      customElementsExportBehavior: "auto-define-custom-elements",
       externalRuntime: false,
-    }
+    },
+    {
+      type: "dist",
+      esmLoaderPath: "../loader", // Creates a loader script
+    },
   ],
   testing: {
     browserHeadless: "new",
